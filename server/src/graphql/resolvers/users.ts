e197d9795aa6aedb168dc9cd7f715f5fe03880f5
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-09-24 18:54:02 -0500

    Update users.ts

--- server/src/graphql/resolvers/users.ts
+++ server/src/graphql/resolvers/users.ts
@@ -1,12 +1,22 @@
 export const users_resolver = {
   Query: {
     users: async (_: any, { amount, offset }: { amount: number; offset: number }) => {
-      return [
-        {
-          username: 'hello',
-          id: 1
-        }
+      const fakeUsers = [
+        { username: 'billy', id: 1 },
+        { username: 'willy', id: 2 },
+        { username: 'jimmmy', id: 3 },
+        { username: 'timmy', id: 4 },
+        { username: 'joe', id: 5 },
+        { username: 'doug', id: 6 },
+        { username: 'frogdude1123', id: 7 }
       ];
+
+      const newArray = [];
+      for (let i = 0; i < amount; i++) {
+        newArray.push(fakeUsers[i + offset]);
+      }
+
+      return newArray;
     }
   }
 };
